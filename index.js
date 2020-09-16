d74150b4db30877dc0f90fd5302d47f295b60770
hery8a <herii8a@gmail.com>
2020-09-16 02:21:58 -0600

    [ADD] se añade express para servir archivos en produccion

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});
+
+app.listen( process.env.PORT, () => {});
