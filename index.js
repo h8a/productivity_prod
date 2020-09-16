8a8f6192c2c775f39274bce30c40412252a27ff5
hery8a <herii8a@gmail.com>
2020-09-16 03:56:35 -0600

    [ADD] se agrega paquete de cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
